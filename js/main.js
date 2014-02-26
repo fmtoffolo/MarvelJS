3d6a5820227e1075810e95c05f9ff115f8a8b28a
fmtoffolo <fmtoffolo+git@gmail.com>
2014-02-26 20:22:22 -0300

    sdds
    sds

--- js/main.js
+++ js/main.js
@@ -11,7 +11,8 @@ var marvelApi = (function($, Handlebars){
             $.getJSON(fullURL, function(data){
                 var data = data.data.results,
                     fullChar = false,
-                    finalData;
+                    finalData,
+                    comicsURL;
                 
                 //loop through response data for character with pic and description. when it finds one, change the fullchar to true and 
                 for (var i = 0; i < data.length; i++) {
@@ -34,10 +35,10 @@ var marvelApi = (function($, Handlebars){
                 //if no character with pic and description is found in the sample of 50, make a new request.
                 if(fullChar == false){
                     return getCharacter();
-                })
+                }
 
                 
-                var comicsURL = 'http://gateway.marvel.com:80/v1/public/characters/' + finalData.id + '/comics?apikey=183da47a6957f77d1e530a4e3ba93528'
+                comicsURL = 'http://gateway.marvel.com:80/v1/public/characters/' + finalData.id + '/comics?apikey=183da47a6957f77d1e530a4e3ba93528'
                 
                 $.getJSON(comicsURL, function(data){
                     var comics = data.data.results;
