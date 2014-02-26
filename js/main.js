4543809f5ec1b45eda7ee29c3633d0c0636955c9
fmtoffolo <fmtoffolo+git@gmail.com>
2014-02-26 19:59:56 -0300

    try
    make it faster and less requests

--- js/main.js
+++ js/main.js
@@ -5,24 +5,32 @@ var marvelApi = (function($, Handlebars){
     var getCharacter = function(){
             var pubKey = '183da47a6957f77d1e530a4e3ba93528',
                 randomChar = Math.floor(Math.random()*1401);
-                fullURL = 'http://gateway.marvel.com:80/v1/public/characters?limit=1&offset=' + randomChar + '&apikey=' + pubKey;
+                fullURL = 'http://gateway.marvel.com:80/v1/public/characters?limit=20&offset=' + randomChar + '&apikey=' + pubKey;
 
 
             $.getJSON(fullURL, function(data){
-                console.log(data);
-                var data = data.data.results[0];
-                console.log(data.name);
-                console.log(data.description);
-                if(data.thumbnail.path === "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available"){
-                    return getCharacter(); 
-                }
-                var finalData = {
-                    name: data.name,
-                    description: data.description,
-                    imgURL: data.thumbnail.path +'.'+ data.thumbnail.extension,
-                    id: data.id
+                var data = data.data.results,
+                    fullChar = false,
+                    var finalData;
+                
+                for (var i = 0; i < data.length; i++) {
+                    if(data[i].data.thumbnail.path !== "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available" && data[i].data.description !== "" && fullChar == false){
+                        
+                        finalData = {
+                            name: data[i].data.name,
+                            description: data[i].data.description,
+                            imgURL: data[i].data.thumbnail.path +'.'+ data[i].data.thumbnail.extension,
+                            id: data.id
+                            }                         
+                        fullChar = true;
                     }
+                };
+                
+                
+
+                
                 var comicsURL = 'http://gateway.marvel.com:80/v1/public/characters/' + finalData.id + '/comics?apikey=183da47a6957f77d1e530a4e3ba93528'
+                
                 $.getJSON(comicsURL, function(data){
                     var comics = data.data.results;
                     var comicsData = [];
