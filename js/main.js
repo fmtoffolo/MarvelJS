9722612eaf5f55eafe0e9abedf1ee0ea3a02a5f8
Fernando Tóffolo <fmtoffolo@gmail.com>
2014-02-26 15:59:28 -0300

    test
    test

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
-var marvelApi = (function(){
+var marvelApi = (function($, Handlebars){
 
-	var pubKey = '183da47a6957f77d1e530a4e3ba93528',
-		randomChar = Math.floor(Math.random()*1401);
-		fullURL = 'http://gateway.marvel.com:80/v1/public/characters?limit=1&offset=' + randomChar + '&apikey=' + pubKey;
+    var pubKey = '183da47a6957f77d1e530a4e3ba93528',
+        randomChar = Math.floor(Math.random()*1401);
+        fullURL = 'http://gateway.marvel.com:80/v1/public/characters?limit=1&offset=' + randomChar + '&apikey=' + pubKey;
 
-		$.getJSON(fullURL, function(data){
-			console.log(data);
-			var data = data.data.results[0];
-			console.log(data.name);
-			console.log(data.description);
-		})
+        $.getJSON(fullURL, function(data){
+            console.log(data);
+            var data = data.data.results[0];
+            console.log(data.name);
+            console.log(data.description);
+            var finalData = {
+                name: data.name,
+                description: data.description,
+                imgURL: data.thumbnail.path + data.thumbnail.extension;
+            }
+            
+            var templateScript = $('#main').html();
+            var theTemplate = Handlebars.compile(templateScript);
+
+            $(document.body).append(theTemplate(finalData));
+            
+        })
 
 
 
-})(jQuery)
+})(jQuery, Handlebars)
