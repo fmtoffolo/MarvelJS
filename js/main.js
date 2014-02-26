a18bc6be0904e187ede23bd7928623e7bbe91aa6
Fernando Tóffolo <fmtoffolo@gmail.com>
2014-02-26 16:27:17 -0300

    fds
    dsf

--- js/main.js
+++ js/main.js
@@ -1,30 +1,33 @@
 var marvelApi = (function($, Handlebars){
 
-    var pubKey = '183da47a6957f77d1e530a4e3ba93528',
-        randomChar = Math.floor(Math.random()*1401);
-        fullURL = 'http://gateway.marvel.com:80/v1/public/characters?limit=1&offset=' + randomChar + '&apikey=' + pubKey;
+    
 
     var getCharacter = function(){
-            $.getJSON(fullURL, function(data){
-            console.log(data);
-            var data = data.data.results[0];
-            console.log(data.name);
-            console.log(data.description);
-            if(data.thumbnail.path === "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available" || data.description === ""){
-                return getCharacter(); 
-            }
-            var finalData = {
-                name: data.name,
-                description: data.description,
-                imgURL: data.thumbnail.path +'.'+ data.thumbnail.extension
-            }
-            
-            var templateScript = $('#main').html();
-            var theTemplate = Handlebars.compile(templateScript);
+            var pubKey = '183da47a6957f77d1e530a4e3ba93528',
+                randomChar = Math.floor(Math.random()*1401);
+                fullURL = 'http://gateway.marvel.com:80/v1/public/characters?limit=1&offset=' + randomChar + '&apikey=' + pubKey;
 
-            $(document.body).append(theTemplate(finalData));
-            
-            })
+
+            $.getJSON(fullURL, function(data){
+                console.log(data);
+                var data = data.data.results[0];
+                console.log(data.name);
+                console.log(data.description);
+                if(data.thumbnail.path === "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available" || data.description === ""){
+                    return getCharacter(); 
+                }
+                var finalData = {
+                    name: data.name,
+                    description: data.description,
+                    imgURL: data.thumbnail.path +'.'+ data.thumbnail.extension
+                }
+                
+                var templateScript = $('#main').html();
+                var theTemplate = Handlebars.compile(templateScript);
+
+                $(document.body).append(theTemplate(finalData));
+                
+                })
         }
 
     getCharacter();
